4e4f4a56a8fb0443342e6304bbf7bf5b3a31b959
maciej-klimek <maciej.klimek09@gmail.com>
2025-01-09 13:00:09 +0100

    client service contenerization, integrated in compose

---
+++ martin.mk
@@ -0,0 +1,31 @@
+DOCKER_COMPOSE_FILE=docker-compose.yml
+
+## up: starts all containers in the background without forcing build
+up:
+	@echo "Starting Docker containers..."
+	docker compose up -d
+	@echo "Docker containers started!"
+
+## up_build: stops docker-compose (if running), builds all images and starts docker compose
+up_build:
+	@echo "Stopping Docker containers (if running)..."
+	docker compose down
+	@echo "Building and starting Docker containers..."
+	docker compose up --build -d
+	@echo "Docker containers built and started!"
+
+## down: stops all containers
+down:
+	@echo "Stopping Docker containers..."
+	docker compose down
+	@echo "Done!"
+
+## logs: shows logs from all services
+logs:
+	@echo "Fetching logs from all services..."
+	docker compose logs -f
+
+init-mongo:
+	@echo "Initializing MongoDB..."
+	docker exec -it mongodb mongoimport --db database --collection transcriptions --file /docker-entrypoint-initdb.d/transcriptions.json --jsonArray -u admin -p password --authenticationDatabase admin
+	@echo "MongoDB initialized!"
